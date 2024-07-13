470713168db3e580a676ee1fa66607673650b500
Anshul-exe <anshulchauhanworks@gmail.com>
2024-07-13 05:38:56 +0530

    LazyVim and Oh-My-Posh

---
+++ nvim/.config/lua/plugins/ui.lua
@@ -0,0 +1,136 @@
+return {
+  {
+    -- messages, cmdline and the popupmenu
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
+      })
+
+      opts.presets.lsp_doc_border = true
+    end,
+  },
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 5000,
+    },
+  },
+
+  -- filename
+  {
+    "b0o/incline.nvim",
+    dependencies = { "craftzdog/solarized-osaka.nvim" },
+    event = "BufReadPre",
+    priority = 1200,
+    config = function()
+      local colors = require("solarized-osaka.colors").setup()
+      require("incline").setup({
+        highlight = {
+          groups = {
+            InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
+            InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
+          },
+        },
+        window = { margin = { vertical = 0, horizontal = 1 } },
+        hide = {
+          cursorline = true,
+        },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          if vim.bo[props.buf].modified then
+            filename = "[+] " .. filename
+          end
+
+          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+          return { { icon, guifg = color }, { " " }, { filename } }
+        end,
+      })
+    end,
+  },
+
+  -- buffer line
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+    },
+    opts = {
+      options = {
+        mode = "tabs",
+        -- separator_style = "slant",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
+  },
+
+  -- animations
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.scroll = {
+        enable = false,
+      }
+    end,
+  },
+
+  {
+    "echasnovski/mini.indentscope",
+    version = false, -- wait till new 0.7.0 release to put it back on semver
+    event = "LazyFile",
+    opts = {
+      -- symbol = "🎀💋🎧🍷🍌"",
+      symbol = "|",
+      -- symbol = "💲",
+      options = { try_as_border = true },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "alpha",
+          "dashboard",
+          "fzf",
+          "help",
+          "lazy",
+          "lazyterm",
+          "mason",
+          "neo-tree",
+          "notify",
+          "toggleterm",
+          "Trouble",
+          "trouble",
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
+  },
+
+  --Welcome logo
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    opts = function(_, opts)
+      local logo = [[
+            █████╗ ███╗   ██╗███████╗██╗  ██╗██╗   ██╗██╗             Z
+      ██╔══██╗████╗  ██║██╔════╝██║  ██║██║   ██║██║         Z
+   ███████║██╔██╗ ██║███████╗███████║██║   ██║██║      z
+ ██╔══██║██║╚██╗██║╚════██║██╔══██║██║   ██║██║    z
+ ██║  ██║██║ ╚████║███████║██║  ██║╚██████╔╝███████╗
+ ╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝
+]]
+
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
+      opts.config.header = vim.split(logo, "\n")
+    end,
+  },
+}
---
+++ nvim/.local/lazy/pkg-cache.lua
@@ -0,0 +1,10 @@
+return {version=12,pkgs={{source="lazy",spec=function()
+return {
+  -- nui.nvim can be lazy loaded
+  { "MunifTanjim/nui.nvim", lazy = true },
+  {
+    "folke/noice.nvim",
+  },
+}
+
+end,name="noice.nvim",dir="C:/Users/anshu/AppData/Local/nvim-data/lazy/noice.nvim",file="lazy.lua",},{source="lazy",spec={"nvim-lua/plenary.nvim",lazy=true,},name="plenary.nvim",dir="C:/Users/anshu/AppData/Local/nvim-data/lazy/plenary.nvim",file="community",},{source="rockspec",spec={"telescope.nvim",build=false,specs={{"nvim-lua/plenary.nvim",lazy=true,},},},name="telescope.nvim",dir="C:/Users/anshu/AppData/Local/nvim-data/lazy/telescope.nvim",file="telescope.nvim-scm-1.rockspec",},},}
